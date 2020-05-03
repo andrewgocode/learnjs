bc302297442755ea1e7fa964a5dc429430aef623
andrewgo2017 <andrewmailmessage@gmail.com>
2020-05-03 17:05:57 +0300

    lesson_11

---
+++ lesson11/js/age.js
@@ -0,0 +1,7 @@
+let age = document.getElementById("age")
+function showUser(surname, name) {
+    console.log(this)
+    alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+
+showUser.call(age, "Иванов", "Иван");
